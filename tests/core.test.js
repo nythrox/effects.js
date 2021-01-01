0ba3952fee140812a78f5567fa3ff110b27a3663
Jason Santiago <jasonsantiagobutler@gmail.com>
2021-01-01 03:27:45 -0300

    cleanup tests

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,4 +1,4 @@
-const { handler, effect, run, resume } = require("../src");
+const { handler, effect, run, resume, pipe, eff } = require("../src");
 
 describe("effect multiple arguments", () => {
   it("should always receive all the arguments passed", async () => {
@@ -11,5 +11,52 @@ describe("effect multiple arguments", () => {
     });
     const res = await pipe(multiply(20, 10), withMultipy, run);
     expect(res).toEqual(200);
+  });
+});
+
+describe("defer actions for later", () => {
+  it("should execute in the correct order", async () => {
+    const defer = effect("defer");
+    const log = effect("log");
+
+    const withDefer = handler({
+      defer: (fn) => {
+        return resume().chain((value) => fn.map(() => value));
+      },
+    });
+    let i = 1;
+    const withLog = handler({
+      log: (msg) => {
+        switch (i) {
+          case 1: {
+            expect(msg).toEqual("loading");
+            break;
+          }
+          case 2: {
+            expect(msg).toEqual("finished");
+            break;
+          }
+          case 3: {
+            expect(msg).toEqual("done2");
+            break;
+          }
+          case 3: {
+            expect(msg).toEqual("done1");
+            break;
+          }
+        }
+        i++;
+        return resume();
+      },
+    });
+
+    const program = eff(function* () {
+      yield defer(log("done1"));
+      yield log("loading");
+      yield defer(log("done2"));
+      yield log("finished");
+    });
+
+    await run(withLog(withDefer(program)));
   });
 });
--- tests/nested.test.js
+++ tests/nested.test.js
@@ -14,50 +14,15 @@ const test3 = effect("test3");
 
 const withTest1 = handler({
   return: (val) => pure(val + "f1"),
-  // test1: (value) =>
-  //   callback((exec, done) => {
-  //     exec(resume(value + "!"))((val) => done("~" + val + "~"));
-  //   }),
   test1: (value) => resume(value + "!").map((val) => "~" + val + "~"),
 });
 const withTest2 = handler({
   return: (val) => pure(val + "f2"),
-  // test2: (value) =>
-  //   callback((exec, done, resume) => {
-  //     resume(value + "!")((val) => done("+" + val + "+"));
-  //     // exec(resume(value + "!"))((val) => done("+" + val + "+"));
-  //     // .chain((val) => finishHandler("+" + val + "+")));
-  //   })
-
-  test2: (value) =>
-    resume(value + "!").chain((val) =>
-      singleCallback((done) => {
-        done("+" + val + "+");
-      })
-    ),
-  // test2: (value) => resume(value + "!").map((val) => "+" + val + "+")
+  test2: (value) => resume(value + "!").map((val) => "+" + val + "+"),
 });
 const withTest3 = handler({
   return: (val) => pure(val + "f3"),
-  // test3: (value) =>
-  //   callback((exec, done, resume) => {
-  //   resume(value + "!")((val) => done("(" + val + ")"));
-  //   // exec(resume(value + "!"))((val) => done("(" + val + ")"));
-  //   // exec(
-  //   //   resume(value + "!")
-  //   //     // owo
-  //   //     .chain((val) => finishHandler("(" + val + ")"))
-  //   //   // owo
-  //   // );
-  // })
-  test3: (value) =>
-    resume(value + "!").chain(
-      (val) =>
-        // singleCallback((done) => {
-        pure("(" + val + ")")
-      // })
-    ),
-  // test3: (value) => resume(value + "!").map((val) => "(" + val + ")")
+  test3: (value) => resume(value + "!").map((val) => "(" + val + ")"),
 });
 
 const programhandlerscopedtest = test1("hi0").chain((hi1) =>
@@ -65,8 +30,14 @@ const programhandlerscopedtest = test1("hi0").chain((hi1) =>
 );
 
 describe("nested handlers", () => {
-  it("should have the correct result",async () => {
-    const res = await pipe(programhandlerscopedtest, withTest1, withTest2, withTest3, run);
-    expect(res).toEqual("(+~hi0!hi2!hi3!f1~f2+f3)")
+  it("should have the correct result", async () => {
+    const res = await pipe(
+      programhandlerscopedtest,
+      withTest1,
+      withTest2,
+      withTest3,
+      run
+    );
+    expect(res).toEqual("(+~hi0!hi2!hi3!f1~f2+f3)");
   });
 });
