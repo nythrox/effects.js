c692cc240e783c78f5f3aa2253d8e84721d124c8
Jason Santiago <jasonsantiagobutler@gmail.com>
2021-01-20 17:59:55 -0300

    tests

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,65 +1,65 @@
 const { handler, effect, run, resume, pipe, eff, pure } = require("../src");
 
-// describe("effect multiple arguments", () => {
-//   it("should always receive all the arguments passed", async () => {
-//     const multiply = effect("multiply");
-//     const withMultipy = handler({
-//       multiply: (value, n, k) => {
-//         expect([value, n]).toEqual([20, 10]);
-//         return resume(k, value * n);
-//       },
-//     });
-//     const res = await pipe(multiply(20, 10), withMultipy, run);
-//     expect(res).toEqual(200);
-//   });
-// });
+describe("effect multiple arguments", () => {
+  it("should always receive all the arguments passed", async () => {
+    const multiply = effect("multiply");
+    const withMultipy = handler({
+      multiply: (value, n, k) => {
+        expect([value, n]).toEqual([20, 10]);
+        return resume(k, value * n);
+      },
+    });
+    const res = await pipe(multiply(20, 10), withMultipy, run);
+    expect(res).toEqual(200);
+  });
+});
 
-// describe("defer actions for later", () => {
-//   it("should execute in the correct order", async () => {
-//     const defer = effect("defer");
-//     const log = effect("log");
+describe("defer actions for later", () => {
+  it("should execute in the correct order", async () => {
+    const defer = effect("defer");
+    const log = effect("log");
 
-//     const withDefer = handler({
-//       defer: (fn, k) => {
-//         return resume(k).chain((value) => fn.map(() => value));
-//       },
-//     });
-//     let i = 1;
-//     const withLog = handler({
-//       log: (msg, k) => {
-//         switch (i) {
-//           case 1: {
-//             expect(msg).toEqual("loading");
-//             break;
-//           }
-//           case 2: {
-//             expect(msg).toEqual("finished");
-//             break;
-//           }
-//           case 3: {
-//             expect(msg).toEqual("done2");
-//             break;
-//           }
-//           case 3: {
-//             expect(msg).toEqual("done1");
-//             break;
-//           }
-//         }
-//         i++;
-//         return resume(k);
-//       },
-//     });
+    const withDefer = handler({
+      defer: (fn, k) => {
+        return resume(k).chain((value) => fn.map(() => value));
+      },
+    });
+    let i = 1;
+    const withLog = handler({
+      log: (msg, k) => {
+        switch (i) {
+          case 1: {
+            expect(msg).toEqual("loading");
+            break;
+          }
+          case 2: {
+            expect(msg).toEqual("finished");
+            break;
+          }
+          case 3: {
+            expect(msg).toEqual("done2");
+            break;
+          }
+          case 3: {
+            expect(msg).toEqual("done1");
+            break;
+          }
+        }
+        i++;
+        return resume(k);
+      },
+    });
 
-//     const program = eff(function* () {
-//       yield defer(log("done1"));
-//       yield log("loading");
-//       yield defer(log("done2"));
-//       yield log("finished");
-//     });
+    const program = eff(function* () {
+      yield defer(log("done1"));
+      yield log("loading");
+      yield defer(log("done2"));
+      yield log("finished");
+    });
 
-//     await run(withLog(withDefer(program)));
-//   });
-// });
+    await run(withLog(withDefer(program)));
+  });
+});
 
 describe("resume", () => {
   test("resume out of handler", async () => {
@@ -91,11 +91,41 @@ describe("resume", () => {
     expect(res2).toEqual("do this later again");
     // you can call resume without adding an extra withLater handler, because the handler
     // was already added to the continution thanks to deep handlers
-    const res3 = await run(resume(callLater, "now2")); 
+    const res3 = await run(resume(callLater, "now2"));
     expect(res3).toEqual({
       result: "now1",
-      result2: "now2"
+      result2: "now2",
     });
+  });
+});
+
+describe("adding new handlers in handlers", () => {
+  it("should add a new handler to the scope", async () => {
+    const updatePrintHandler = effect("updatePrintHandler");
+    const print = effect("print");
+    const withPrint = handler({
+      print: (value, k) => {
+        expect(value).toEqual(1);
+        return resume(k);
+      },
+    });
+    const withPrint2 = handler({
+      print: (value, k) => {
+        expect(value).toEqual(2);
+        return resume(k);
+      },
+    });
+    const withUpdatePrintHandler = handler({
+      updatePrintHandler: (k) => {
+        return withPrint2(resume(k));
+      },
+    });
+    const program = eff(function* () {
+      yield print(1);
+      yield updatePrintHandler();
+      yield print(2);
+    });
+    await run(withPrint(withUpdatePrintHandler(program)));
   });
 });
 
@@ -145,6 +175,6 @@ describe("resume", () => {
 //         yield log("finishing number", id);
 //       });
 
-//     run(withLog(schedule(tree(0, 3))));
+//     run(withLog(schedule(tree(0, 2))));
 //   });
 // });
